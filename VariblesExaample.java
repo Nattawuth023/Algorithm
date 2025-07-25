1aaa9dc8dfc54d07e27991852a306d29304f945a
Nattawuth023 <684259023@webmail.npru.ac.th>
2025-07-25 13:01:10 +0700

    Add files via upload

---
+++ VariblesExaample.java
@@ -0,0 +1,30 @@
+public class VariblesExaample {
+    public static void main(String[] args) {
+
+        char clothingSize = 's';//สามารถเป็น 's', 'm', หรือ 'l'
+
+        int websiteVisiyors = 0;//จำนวนคนที่ที่เข้าชมเว็บไซต์
+
+        char studentGrade = 'A';//สามารถเป็น 'A', 'B', 'C', 'D', หรือ 'E'
+
+        double studentGPA = 4.0;// ค่าเกรดเฉลี่ยของนักศึกษา
+
+        final double PI = 3.14159;//ค่า Pi ควรเป็นค่าคงที่
+
+        float annualExpenses = 15000.00f;//ค่าใช้จ่ายรายปีของนักศึกษา
+
+        final int MONTHS_IN_YEAR = 12;//จำนวนเดือนในหนึ่งปี ควรเป็นค่าคงที่
+
+
+        //ตัวอย่างการแสดงผลตัวแปร
+        System.out.println("Clothing Size: "+ clothingSize);
+        System.out.println("Website Visitors: " +websiteVisiyors);
+        System.out.println("student Grade: " + studentGrade);
+        System.out.println("student GPA: " + studentGPA);
+        System.out.println("Value of Pi: " + PI);
+        System.out.println("Annual Expensese: " + annualExpenses);
+        System.out.println("Months in a Year: " + MONTHS_IN_YEAR);
+
+    
+    }
+}
