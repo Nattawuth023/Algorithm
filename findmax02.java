0a24b88ae5f15d4e50f774edb06f3ee6da9337d4
Nattawuth023 <684259023@webmail.npru.ac.th>
2026-01-09 16:59:39 +0700

    Add files via upload

---
+++ findmax02.java
@@ -0,0 +1,10 @@
+public class findmax02 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 555;
+        int c = 25;
+
+        System.out.println("Max = " + Math.max(Math.max(a, b), c));
+
+    }
+}
